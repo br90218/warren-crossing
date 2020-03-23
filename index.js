644d14bdcd7199b4d2fa18cd454f0ed7f789b59f
Brian Teng <askmeifyouwantmyusername@gmail.com>
2020-03-23 13:54:17 -0700

    setting up command args array

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ const Redis = require ('redis');
 const client = new Discord.Client();
 
 const redisClient = Redis.createClient(process.env.REDIS_URL);
-
+const prefix = process.env.PREFIX;
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', async message => {
-	if (message.content === '!ping') {
-		message.channel.send('Pong.');
-	}
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	else if (message.content === 'test'){
-		redisClient.get(message.author, (error, reply) => {
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command.content === 'getprice'){
+		redisClient.get(args[0], (error, reply) => {
 			if(!error && reply)	{
 				message.channel.send(message.author + '\'s island is buying turnips at ' + reply + ' bells!')
 			}
 			else{
-				message.channel.send(`${message.author} has not reported their turnip price of the day. bad bad!`)
+				message.channel.send(`${args[0]} has not reported their turnip price of the day. bad bad!`)
 			}
 		})
 	}
-
-
 });
 
 client.login(process.env.BOT_TOKEN);
